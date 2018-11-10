e56326404ce44bb7ecbb01058c46392dc41ffb16
ChiLi_Lin <904981663@163.com>
2018-11-10 23:49:51 +0800

    优化
    1. 标记上一次更新的块偏移，避免重复更新
    2. 添加层循环次数，避免玩家跑太远，数据越界

--- Assets/Scripts/MapLoopMgr.cs
+++ Assets/Scripts/MapLoopMgr.cs
@@ -5,19 +5,23 @@ using UnityEngine;
 public class MapLoopMgr : MonoBehaviour
 {
     public GameObject player;
+    public int maxLayerRange = 3;       //层循环次数，超过回退，避免数据越界
     public Vector2 blockSize = new Vector2(10, 10);
     public Vector2 blockCount = new Vector2(4, 1);
     public List<GameObject> blockList = new List<GameObject>();
-    
+
+    private Vector2 lastBlockOffset;    //标记上一次更新的块偏移
     private int blockListCount = 0;
     private Vector2 startBlockIdx;
     private Vector2 layerSize;
+    private Vector3 startPos;
 
     void Start()
     {
         blockListCount = blockList.Count;
         startBlockIdx = GetBlockIndex(player);
         layerSize = blockSize * blockCount;
+        startPos = player.transform.position;
     }
 
     private Vector2 blockOffset;
@@ -27,14 +31,21 @@ public class MapLoopMgr : MonoBehaviour
     private Vector2 temVec2;
     void Update()
     {
+        //优化，避免玩家跑太远，数据越界
+        ClampPlayerInRange();
+
         //玩家当前块和最开始块的偏移量
         blockOffset = GetBlockIndex(player) - startBlockIdx;
+        if (lastBlockOffset == blockOffset)         // 优化，避免重复更新
+            return;
+        lastBlockOffset = blockOffset;
+
         //块在这个层的内的偏移量
         blockInLayerOffset = new Vector2(blockOffset.x % blockCount.x, blockOffset.y % blockCount.y);
         //层的偏移量
         layerOffset = new Vector2((int)(blockOffset.x / blockCount.x), (int)(blockOffset.y / blockCount.y));
 
-        //Debug.Log("blockOffset :" + blockOffset + "   sectionOffset:" + sectionOffset + "  blockInSectionOffset:" + blockInSectionOffset);
+        Debug.Log("blockOffset :" + blockOffset + "   layerOffset:" + layerOffset + "  blockInLayerOffset:" + blockInLayerOffset);
 
         float newX, newZ;
         for (int i = 0; i < blockListCount; i++)
@@ -54,7 +65,7 @@ public class MapLoopMgr : MonoBehaviour
             else if (blockOffset.y < 0 && temVec2.y > (blockCount.y + blockInLayerOffset.y - 1))
                 newZ = layerSize.y * (layerOffset.y - 1);
             else
-                newZ = layerSize.y * (layerOffset.y );
+                newZ = layerSize.y * (layerOffset.y);
 
             temGO.transform.position = new Vector3(newX, temGO.transform.position.y, newZ);
         }
@@ -87,6 +98,27 @@ public class MapLoopMgr : MonoBehaviour
 
         return new Vector2((int)(vec.x), (int)(vec.y));
     }
-    
+
+    private Vector3 temVec3;
+    private float temFloatX;
+    private float temFloatZ;
+    private void ClampPlayerInRange()
+    {
+        temVec3 = player.transform.position;
+        temFloatX = temVec3.x;
+        temFloatZ = temVec3.z;
+        if (temVec3.x > layerSize.x * maxLayerRange + startPos.x ||
+            temVec3.x < -layerSize.x * maxLayerRange + startPos.x)
+        {
+            temFloatX = startPos.x;
+        }
+        if (temVec3.z > layerSize.y * maxLayerRange + startPos.z ||
+            temVec3.z < -layerSize.y * maxLayerRange + startPos.z)
+        {
+            temFloatZ = startPos.z;
+        }
+        player.transform.position = new Vector3(temFloatX, player.transform.position.y, temFloatZ);
+    }
+
 
 }
